e07708f143b9dd52dc82c388d7e6063972dc55d3
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-06-29 10:14:55 +0100

    connected sql to postgres db, fixed syntax errors

--- server/database/theatre.sql
+++ server/database/theatre.sql
@@ -1,31 +1,31 @@
-DROP TABLE IF EXISTS theatre, shows, screens, screenings
+DROP TABLE IF EXISTS theatre, shows, screens, screenings ;
 
-CREATE TABLE theatre {
-    theatre_id INT SERIAL PRIMARY KEY
-    theatre_name VARCHAR NOT NULL
+CREATE TABLE theatre (
+    theatre_id SERIAL PRIMARY KEY,
+    theatre_name VARCHAR NOT NULL,
     address VARCHAR
-}
+);
 
-CREATE TABLE shows {
-    show_id INT SERIAL PRIMARY KEY
-    show_name VARCHAR NOT NULL
-    genre VARCHAR
-    rating INT
-    poster_image_url VARCHAR
-    screen_id INT REFERENCES screens.screen_id 
-}
-
-CREATE TABLE screens {
-    screen_id INT SERIAL PRIMARY KEY
-    capacity INT
+CREATE TABLE screens (
+    screen_id SERIAL PRIMARY KEY,
+    capacity INT,
     remaining_seats INT 
-}
+);
 
-CREATE TABLE screenings {
-    screening_id INT SERIAL PRIMARY KEY
-    show_id INT REFERENCES shows.show_id
-    screen_id INT REFERENCES screens.screen_id
-    show_time TIME(HH:MI)
-    show_date DATE 
-}
+CREATE TABLE shows (
+    show_id SERIAL PRIMARY KEY,
+    show_name VARCHAR NOT NULL,
+    genre VARCHAR,
+    rating INT,
+    poster_image_url VARCHAR,
+    screen_id INT REFERENCES screens(screen_id)
+);
+
+CREATE TABLE screenings (
+    screening_id SERIAL PRIMARY KEY,
+    show_time TIME,
+    show_date DATE,
+    show_id INT REFERENCES shows(show_id),
+    screen_id INT REFERENCES screens(screen_id)
+);
 
